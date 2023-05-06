c752f37a201ba8195dfa8e852f893aa1f651b2e3
mirindi19 <mirindisaidi19@gmail.com>
2023-05-06 13:54:30 -0700

    ft(table)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
-import Home from './pages/Home';
+// import Home from './pages/Home';
+// import User from './components/table/User';
+// import Commitee from './components/table/Commitee';
+// import Schools from './components/table/Schools';
+// import Families from './components/table/Families';
+import Topnav from './components/topnav/Topnav';
 function App() {
   return (
     <div className="App">
-    <Home/>
+    <Topnav/>
     </div>
   );
 }
---
+++ src/components/table/Families.js
@@ -0,0 +1,109 @@
+import "./families.scss"
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Siderbar from '../sidebar/Sidebar';
+import Navbar from '../navbar/Nav';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+  }
+  
+  const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+  ];
+  
+const Families = () => {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+  
+  return (
+    <div className="families">
+    <Siderbar/>
+    <div className="navbarfamilies">
+    <Navbar/>
+    <div className="familiesTable">
+    <Button variant="outlined" onClick={handleClickOpen} className="familiesbtn">
+    Add Families
+  </Button>
+  <Dialog open={open} onClose={handleClose}>
+    <DialogTitle>Subscribe</DialogTitle>
+    <DialogContent>
+      <DialogContentText>
+        To subscribe to this website, please enter your email address here. We
+        will send updates occasionally.
+      </DialogContentText>
+      <TextField
+        autoFocus
+        margin="dense"
+        id="name"
+        label="Email Address"
+        type="email"
+        fullWidth
+        variant="standard"
+      />
+    </DialogContent>
+    <DialogActions>
+      <Button onClick={handleClose}>Cancel</Button>
+      <Button onClick={handleClose}>Subscribe</Button>
+    </DialogActions>
+  </Dialog>
+    <TableContainer component={Paper} className="savetablefamilies">
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    </div>
+    </div>
+    </div>
+  )
+}
+
+export default Families
